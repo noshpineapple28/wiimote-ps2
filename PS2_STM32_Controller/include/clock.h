287fae9b3524079b1bba6b2a43f5482c6c3a5e60
noshpineapple28 <manoucherin@gmail.com>
2025-03-02 21:16:53 -0500

    chore: remove dead/unusable code (sad)

--- PS2_STM32_Controller/include/clock.h
+++
@@ -1,6 +0,0 @@
-#ifndef __CLOCK_H
-#define __CLOCK_H
-
-void System_Clock_Init(void);
-
-#endif
--- Wiimote_Puppet_BLE_Arduino_Adapter/include/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MAIN_H_
-#define __MAIN_H_
-
-#include <Arduino.h>
-#include <stdint.h>
-
-#endif
